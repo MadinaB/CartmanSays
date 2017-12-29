673a2dff4b605e61d3fb77fcd4e3ab61c5556246
Madina <madinabektayeva@gmail.com>
2017-12-29 22:48:29 +0900

    now Cartman speaks a little

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-
 var CartmanQuotes = ['I am fat, but you are ugly. I can diet.' , 
 'I am big-boned.' , 
 'I just have not grown into my body yet.',
 'I just have sweet hockey body',
 'I am festively plump'];
+var CartmanSays = ['festively_plump', 'big_boned'];
 var r = 0;
 var CartmanQuotesSize = 5;
-
+var name=CartmanSays[1];
 function CartmanSay() {
     var r = parseInt((Math.random()*100)%CartmanQuotesSize);
     new Notification(CartmanQuotes[r],{icon: 'Cartman.png'});
+    var audio = (new Audio('audio/'+name+'.mp3'));
+    audio.volume=0.2;
+    audio.play();
 }
 if (window.Notification) {
-    setInterval(CartmanSay, 1000);
+    setInterval(CartmanSay, 10000);
 }
